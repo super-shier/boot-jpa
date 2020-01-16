0b7beaa70734ddb1d705e7e3ae03a64765724b35
liyunbiao <liyunbiao@51xf.cn>
2020-01-16 15:51:21 +0800

    init

--- src/main/java/com/shier/common/boot/jpa/common/config/Swagger2Config.java
+++ src/main/java/com/shier/common/boot/jpa/common/config/Swagger2Config.java
@@ -11,7 +11,7 @@ import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
- * @author alex
+ * @author liyunbiao
  * @date 2019/3/11 11:05 AM
  */
 @Configuration
